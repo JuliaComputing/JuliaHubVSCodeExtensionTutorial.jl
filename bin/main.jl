029a7564075e7c6fc80bb6c3bfca3854e5ddfc94
jeremie.db <jeremie.db@evovest.com>
2022-01-06 16:28:59 -0500

    up

---
+++ bin/main.jl
@@ -0,0 +1,23 @@
+using SecretAnalysis, JSON3, Printf, Distributed
+
+n = parse(Int64, replace(get(ENV, "n", "10_000_000_000"), '_' => ""))
+
+stats = @timed begin
+    estimate_pi_distributed(n)
+end
+
+@info "Finished computation. π estimate: " stats[:value]
+
+results = Dict(
+    :pi => stats[:value],
+    :num_trials => n,
+    :compute_time => stats[:time],
+    :workers => nworkers()
+)
+
+open("results.json", "w") do io
+    JSON3.pretty(io, results)
+end
+
+ENV["RESULTS"] = JSON3.write(results)
+ENV["RESULTS_FILE"] = "results.json"
